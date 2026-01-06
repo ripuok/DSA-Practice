a73262e66e19ca8ee95d5a089c8b2feee9ef350c
Ripudaman Singh <ripuok@gmail.com>
2026-01-07 00:00:49 +0530

    Create HOF.js with HOF functions documentation
    Add HOF.js with documentation for higher-order functions

---
+++ HOF.js
@@ -0,0 +1,33 @@
+/**
+HOF functions and thier base itertions
+map
+reduce
+filter
+find
+findIndex
+some
+every
+has
+
+sort (high difficulty)
+
+| HOF    | Stops Early | Returns      | Core Use    |
+| ------ | ----------- | ------------ | ----------- |
+| map    | ❌           | new array    | transform   |
+| filter | ❌           | new array    | select      |
+| reduce | ❌           | single value | aggregate   |
+| sort   | ❌           | same array   | order       |
+| some   | ✅           | boolean      | any         |
+| every  | ✅           | boolean      | all         |
+| find   | ✅           | value        | first match |
+
+*/
+
+function map(){
+  let response=[];
+
+  
+}
+
+
+
